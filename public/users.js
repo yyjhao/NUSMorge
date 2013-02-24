eccef5748a5130465dd5421bdf1e67afe40fbb2e
yyjhao <yyjhao@gmail.com>
2013-02-24 20:12:57 +0800

    show hide a user

--- public/users.js
+++ public/users.js
@@ -24,28 +24,33 @@ var UserBar = function(userList, timetable, userInfo){
 	var users = {};
 	var editing = false;
 	view.addUser = function(name, url, success){
+		var user = {};
+		user.hidden = false;
 		parseUrl(url, function(mods){
 			var id = name + "_" + (new Date());
 			timetable.addUser(id, ["a", "b"]);
 			var elm = document.createElement("li"),
+				viewToggle = document.createElement("span");
 				nameDisplay = document.createElement("span"),
-				remove = document.createElement("span");
+				remove = document.createElement("div");
+			viewToggle.className = "view-toggle toggled";
 			nameDisplay.className = "name";
 			remove.className = "toremove";
 			remove.innerHTML = "x";
 			nameDisplay.innerHTML = name;
+			elm.appendChild(viewToggle);
 			elm.appendChild(nameDisplay);
 			elm.appendChild(remove);
 			$(elm).hover(function(){
-				if(!editing) timetable.highlightUser(id);
+				if(!editing && !user.hidden) timetable.highlightUser(id);
 			}, function(){
-				if(!editing) timetable.hideUserView();
+				if(!editing && !user.hidden) timetable.hideUserView();
 			}).click(function(){
-				if(editing == id){
+				if(editing == id && !user.hidden){
 					timetable.hideUserView();
 					$(this).removeClass("editing");
 					editing = false;
-				}else if(!editing){
+				}else if(!editing && !user.hidden){
 					editing = id;
 					$(this).addClass("editing");
 					timetable.editUser(id);
@@ -59,6 +64,21 @@ var UserBar = function(userList, timetable, userInfo){
 						editing = false;
 					}
 					userList.removeChild(elm);
+				}
+				e.preventDefault();
+				return false;
+			});
+			$(viewToggle).click(function(e){
+				if(editing == id)return;
+				timetable.hideUserView();
+				if(user.hidden){
+					$(this).addClass("toggled");
+					user.hidden = false;
+					timetable.showUser(id);
+				}else{
+					$(this).removeClass("toggled");
+					user.hidden = true;
+					timetable.hideUser(id);
 				}
 				e.preventDefault();
 				return false;
