b49fd45f0d16dd532664282f80c6f96db13ebfd1
yyjhao <yyjhao@gmail.com>
2013-02-24 12:46:44 +0800

    Initial script templates

---
+++ frontend/users.js
@@ -0,0 +1,23 @@
+/*
+ * the user side bar io
+ * adding new users
+ * parsing urls
+ * hovering and shit
+ */
+
+var userManagement = function(userList, timetable){
+	// parse url to an array of modules
+	//  in the form of [CS1234=L2S2]
+	//  may want to further parse
+	function parseUrl(url){
+		url.split("#").pop().split("&");
+	}
+
+	var view = {};
+
+	// add methods to view
+	
+
+	return view;
+};
+
